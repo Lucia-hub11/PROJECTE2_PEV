b30493022db2fb2763ec7ff452b3814dc1c19335
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2024-11-23 12:51:36 +0100

    Moviment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,11 @@ public class PlayerMovement : MonoBehaviour
     InputControllers _inputs;
     GroundChecker _groundChecker;
 
-
     public float WalkSpeed = 5;
-    public float JumpSpeed = 10;
+    public float JumpSpeed = 5;
     public float AirControl = 0.1f;
-    public float TurnSpeed = 1;
 
     private Vector3 _lastVelocity;
-
 
     // Start is called before the first frame update
     void Start()
@@ -44,35 +41,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
+        // Calcular la direcci?n de entrada (incluye movimiento lateral y hacia adelante/atr?s)
         var localInput = transform.right * _inputs.Move.x + transform.forward * _inputs.Move.y;
-        Vector3 direction = new Vector3(localInput.x, 0, localInput.z); //A y D giren el personatge
+        Vector3 direction = new Vector3(localInput.x, 0, localInput.z);
 
         Vector3 velocity = new Vector3();
 
+        // Control en el aire o en tierra
         float smoothFactor = _groundChecker.Grounded ? 1 : AirControl * Time.deltaTime;
 
-        //float currentSpeed = _inputs.RunStart ? RunSpeed : WalkSpeed;
+        // Aplicar velocidad horizontal (sin giro)
+        velocity.x = Mathf.Lerp(_lastVelocity.x, direction.x * WalkSpeed, smoothFactor);
+        velocity.z = Mathf.Lerp(_lastVelocity.z, direction.z * WalkSpeed, smoothFactor);
 
-
-        //velocity.x = Mathf.Lerp(_lastVelocity.x, direction.x * currentSpeed, smoothFactor);
-        //velocity.y = _lastVelocity.y;
-        //velocity.z = Mathf.Lerp(_lastVelocity.z, direction.z * currentSpeed, smoothFactor);
-
+        // Aplicar gravedad
         velocity.y = GetGravity();
+
+        // Aplicar salto si corresponde
         if (ShouldJump())
             Jump(ref velocity);
 
-
+        // Mover al personaje usando el CharacterController
         _characterController.Move(velocity * Time.deltaTime);
 
-
-        if (direction.magnitude > 0)
-        {
-            Vector3 target = transform.position + direction;
-
-            Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
-        }
+        // Guardar la ?ltima velocidad para la f?sica
         _lastVelocity = velocity;
     }
 
