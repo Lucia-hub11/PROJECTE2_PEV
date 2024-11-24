323b6d22a57bbf24e792dbc4e9c20853c114071a
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-11-24 18:57:33 +0100

    spawn

---
+++ Assets/Shooter.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject Prefab;
+    private Rigidbody _rg;
+    public float bulletspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rg = GetComponent<Rigidbody>();
+        Applyspeed();
+        DestroyBullet();
+    }
+    private void DestroyBullet()
+    {
+        Destroy(gameObject, 1);
+    }
+
+    private void Applyspeed()
+    {
+        _rg.velocity = transform.forward * bulletspeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            ShouldShoot();
+    }
+
+    private void ShouldShoot()
+    {
+        SpawnBullet();
+    }
+
+    private void SpawnBullet()
+    {
+        Instantiate(Prefab, transform.position, Quaternion.identity);
+    }
+}
