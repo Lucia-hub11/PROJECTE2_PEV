871378450f9a33bfa24f9fc59f1eaeedf8a9e3e3
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-12-07 18:13:17 +0100

    CONFETTI!!

--- Assets/Scripts/EnemyDestruction.cs
+++ Assets/Scripts/EnemyDestruction.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemyDestruction : MonoBehaviour
 {
+    private Rigidbody _rg;
+    public GameObject Explosion;
+    private ScreenEffect screenEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rg = GetComponent<Rigidbody>();
+        screenEffect = FindObjectOfType<ScreenEffect>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject ExplosionSystem = Instantiate(Explosion, transform.position, Quaternion.identity);
+        if (screenEffect != null)
+        {
+            screenEffect.OnObjectDestroyed();
+        }
+        Destroy(gameObject);
+        Destroy(ExplosionSystem, 1f);
+    }
+
+    void OnDestroy()
+    {
+        FindObjectOfType<ScreenEffect>().OnObjectDestroyed();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
