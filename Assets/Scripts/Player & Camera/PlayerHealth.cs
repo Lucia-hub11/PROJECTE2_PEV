564a86028723e77eae1993b848399e0a231490ad
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-12-29 19:57:09 +0100

    healthyyy

--- Assets/Scripts/Player & Camera/PlayerHealth.cs
+++ Assets/Scripts/Player & Camera/PlayerHealth.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    //public float maxHealth = 100;
-    public float currentHealth = 100;
+    public float maxHealth = 100;
+    private float currentHealth;
+
+    void start()
+    {
+        currentHealth = 200;
+
+    }
 
     public void TakeDamage(float amount)
     {
         //Debug.Log("DAMAGE " + amount);
         currentHealth = currentHealth - amount;
-
+        Debug.Log("CURRENT H " + currentHealth);
         if (currentHealth == 0)
         {
             //muerte
