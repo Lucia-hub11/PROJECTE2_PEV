2117d2e6f877b683c3a0e8be1004fd34d2e41f7f
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-12-30 12:38:05 +0100

    i mean es un boss

---
+++ Assets/Scripts/Enemies/BOSS.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOSS : MonoBehaviour
+{
+    private Rigidbody _rg;
+    public GameObject Explosion;
+    private ScreenEffect screenEffect;
+    private WaterBlood waterBlood;
+
+    //audio
+    public static Action OnParty;
+
+
+    void Start()
+    {
+        _rg = GetComponent<Rigidbody>();
+        screenEffect = FindObjectOfType<ScreenEffect>();
+        waterBlood = FindObjectOfType<WaterBlood>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //if (screenEffect != null)
+        //{
+        //    screenEffect.OnObjectDestroyed();
+        //    waterBlood.OnObjectDestroyed();
+        //}
+
+        if (collision.tag == "Bullet")
+        {
+            GameObject ExplosionSystem = Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(ExplosionSystem, 1f);
+            OnParty?.Invoke();
+        }
+        if (collision.tag == "Player")
+        {
+            var healthComponent = collision.GetComponent<PlayerHealth>();
+            if (healthComponent != null)
+            {
+                healthComponent.TakeDamage(5);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (screenEffect != null)
+        {
+            screenEffect.OnObjectDestroyed();
+        }
+
+        if (waterBlood != null)
+        {
+            waterBlood.OnObjectDestroyed();
+        }
+    }
+
+}
