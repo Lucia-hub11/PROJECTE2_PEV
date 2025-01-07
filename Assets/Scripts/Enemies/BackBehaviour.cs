7341f4a56f341856d60071be25ccd3c9f6241878
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-01-07 17:22:44 +0100

    chase i back enemy conills ben fet!

--- Assets/Scripts/Enemies/BackBehaviour.cs
+++ Assets/Scripts/Enemies/BackBehaviour.cs
@@ -6,57 +6,26 @@ public class BackBehaviour : StateMachineBehaviour
 {
     public float backSpeed = 2f;   
     public float backDistance = 3f; 
-    /*private float distanceMoved = 0f;*/
     private Vector3 startPosition;
     private Vector3 backTargetPosition;
-    private bool isBacking = false;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //distanceMoved = 0f;
         startPosition = animator.transform.position;
         backTargetPosition = startPosition - animator.transform.forward * backDistance;
-        isBacking = true;
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (isBacking)
+        animator.transform.position = Vector3.MoveTowards(animator.transform.position, backTargetPosition, backSpeed * Time.deltaTime);
+        if (Vector3.Distance(animator.transform.position, backTargetPosition) < 0.1f)
         {
-            animator.transform.position = Vector3.MoveTowards(
-            animator.transform.position,
-            backTargetPosition,
-            backSpeed * Time.deltaTime
-            );
-            if (Vector3.Distance(animator.transform.position, backTargetPosition) < 0.1f)
-            {
-                isBacking = false;
-                //animator.SetBool("goBack", false);
-                animator.SetBool("isChasing", true);
-            }
+            //animator.SetBool("goBack", false);
+            animator.SetBool("isChasing", true);
         }
-        
-        //Vector3 backDirection = -animator.transform.forward;
-        //Vector3 targetPosition = startPosition + backDirection * backDistance;
-        //animator.transform.position = Vector3.MoveTowards(animator.transform.position, targetPosition, backSpeed * Time.deltaTime);
-
-        //float moveAmount = backSpeed * Time.deltaTime; // Distància que es mourà en cada frame
-        //animator.transform.Translate(backDirection * moveAmount, Space.World);
-        //distanceMoved += moveAmount; // Augmentar la distància recorreguda
-
-
-        // Si l'enemic ha recorregut la distància especificada, tornar al Chase
-        //if (distanceMoved >= backDistance)
-        //{
-        //    animator.SetBool("goBack", false); // Tornar a "Chase"
-        //}
-        
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        isBacking = false;
         animator.SetBool("goBack", false);
     }
 
--- Assets/Scripts/Enemies/ChaseBehaviour.cs
+++ Assets/Scripts/Enemies/ChaseBehaviour.cs
@@ -13,52 +13,25 @@ public class ChaseBehaviour : StateMachineBehaviour
     {
         Vision vision = animator.GetComponent<Vision>();
         WP_target = vision.WayPoint;
-        animator.SetBool("isChasing", true);
     }
     
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
         EnemyDestruction enemyDestruction = animator.GetComponent<EnemyDestruction>();
         bool playerHere = enemyDestruction.IsPlayerHere();
         if (!playerHere)
         {
             Move(animator, WP_target);
-             
         }
         else
         {
             animator.SetBool("goBack", true);
-            animator.SetBool("isChasing", false);
-            //return;
+            animator.SetBool("isChasing", false); 
         }
-
-        
 
     }
     private void Move(Animator animator, Transform target)
     {
-        //if (target == null) return;
-
-        //Vector3 targetPosition = target.position;
-        //if (Move2D)
-        //{
-        //    targetPosition.y = animator.transform.position.y; // Bloqueja el moviment vertical
-        //}
-
-        //Vector3 direction = (targetPosition - animator.transform.position).normalized;
-
-        //// Gira l'enemic cap a la direcció
-        //animator.transform.rotation = Quaternion.LookRotation(direction);
-
-        //// Mou cap al jugador
-        //animator.transform.position += direction * Speed * Time.deltaTime;
-        ////animator.transform.position = Vector3.MoveTowards(
-        ////    animator.transform.position,
-        ////    targetPosition,
-        ////    Speed * Time.deltaTime
-        ////);
-
         Vector3 targetPosition = target.position;
         if (Move2D) //si es vol moure nomes en la y (no jump)
             targetPosition.y = animator.transform.position.y;
@@ -66,9 +39,9 @@ public class ChaseBehaviour : StateMachineBehaviour
         animator.transform.Translate(animator.transform.forward * Speed * Time.deltaTime, Space.World);
     }
 
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        //animator.SetBool("goBack", false);
-        animator.SetBool("isChasing", false);
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    //animator.SetBool("goBack", false);
+    //    animator.SetBool("isChasing", false);
+    //}
 }
