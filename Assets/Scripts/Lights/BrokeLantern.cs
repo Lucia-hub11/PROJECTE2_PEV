b3d69468340c128bbc482241f38eee1fa5c83a1a
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-01-07 21:26:07 +0100

    lantern en proces

---
+++ Assets/Scripts/Lights/BrokeLantern.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokeLantern : MonoBehaviour
+{
+    public float LanternRange = 4;
+    public Transform WayPoint;
+    public Vector3 Offset;
+
+    public Animator LanternAnimator;
+    bool broke_lantern;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position + Offset, LanternRange);
+    }
+
+    void Start()
+    {
+        broke_lantern = false;
+    }
+
+    private int EnemiesDestroyed = 0;
+    public void OnObjectDestroyed()
+    {
+        EnemiesDestroyed += 1;
+    }
+
+    void Update()
+    {
+        if (IsLanternDetected() && EnemiesDestroyed >= 4)
+        {
+            broke_lantern = true;
+            LanternAnimator.SetBool("Near Lantern", broke_lantern);
+        }
+    }
+
+    public bool IsLanternDetected()
+    {
+        return IsInLanternRange(WayPoint);
+    }
+
+    private bool IsInLanternRange(Transform target)
+    {
+        return Vector3.Distance(transform.position + Offset, target.position) < LanternRange;
+    }
+}
